3f579ef72193380601cf07f3246fb929ac5c4977
Lu0-Wenfeng <w2luo@ucsd.edu>
2025-07-30 01:36:23 -0700

    chore: misc

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,15 @@
-const CracoAlias = require('craco-alias');
+const CracoAlias = require("craco-alias");
 
 module.exports = {
   plugins: [
+    // Path aliases
     {
       plugin: CracoAlias,
       options: {
-        source: 'tsconfig',
-        baseUrl: './',
-        tsConfigPath: './tsconfig.json'
-      }
-    }
-  ]
+        source: "tsconfig",
+        baseUrl: "./",
+        tsConfigPath: "./tsconfig.json",
+      },
+    },
+  ],
 };
